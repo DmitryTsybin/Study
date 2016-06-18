100fc029b8a9755e2a8be34b925606451068f23a
Dmitry Tsybin <dmitry.tsybin@gmail.com>
2016-06-19 00:50:44 +0300

    init commit for gopl exercises

---
+++ golp.io/ch1/lissajous/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "image"
+    "image/color"
+    "image/gif"
+    "io"
+    "math"
+    "math/rand"
+    "os"
+)
+
+import (
+    "log"
+    "net/http"
+    "time"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+    )
